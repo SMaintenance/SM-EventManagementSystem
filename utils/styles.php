2f3a5807ffba4c687fa7eb713b62ddc8a618461a
haneyiskdr <haneyiskandar00@gmail.com>
2022-12-27 14:24:59 +0800

    Adding new file for css

--- utils/styles.php
+++ utils/styles.php
@@ -1,7 +1,7 @@
-<link rel = "stylesheet" type = "text/css" href = "css/bootstrap-theme.css">
-<link rel = "stylesheet" type = "text/css" href = "css/bootstrap-theme.css.map">
-<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css">
-<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css.map">
-<link rel = "stylesheet" type = "text/css" href = "css/style.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css.map">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css.map">
+<link rel="stylesheet" type="text/css" href="css/new-style.css">
 <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
 <link rel="icon" href="images/favicon.png" sizes="32x32" type="image/png">
