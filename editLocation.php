5f8a5e19efc28e8b7bf05690f1faefb40af7f922
haneyiskdr <haneyiskandar00@gmail.com>
2023-01-01 20:30:20 +0800

    Make edit location form function

--- editLocation.php
+++ editLocation.php
@@ -3,21 +3,44 @@ require_once 'classes/Location.php';
 require_once 'classes/LocationTableGateway.php';
 require_once 'classes/Connection.php';
 
-$id = $_POST['id'];
-$locationName = $_POST['Name'];
-$locationAddress = $_POST['Address'];
-$managerFName = $_POST['ManagerFName'];
-$managerLName = $_POST['ManagerLName'];
-$managerEmail = $_POST['ManagerEmail'];
-$managerNumber = $_POST['ManagerNumber'];
-$locationMaxCap = $_POST['MaxCapacity'];
+if (isset($_POST['submit'])) {
+    $id = $_POST['id'];
+    $locationName = $_POST['Name'];
+    $locationAddress = $_POST['Address'];
+    $managerFName = $_POST['ManagerFName'];
+    $managerLName = $_POST['ManagerLName'];
+    $managerEmail = $_POST['ManagerEmail'];
+    $managerNumber = $_POST['ManagerNumber'];
+    $locationMaxCap = $_POST['MaxCapacity'];
+    $locationType = $_POST['lType'];
+    $seat = $_POST['seat'];
+    $facilities = $_POST['facilities'];
+    $url = $_POST['link'];
+    $old_image = $_POST['old_image'];
 
-$location = new Location($id, $locationName, $locationAddress, $managerFName, $managerLName,  $managerEmail, $managerNumber, $locationMaxCap);
+    if (isset($_FILES['image']['name']) && ($_FILES['image']['name'] != "")) {
+        $target_dir = "uploads/";
+        $file = $_FILES['image']['name'];
+        $path = pathinfo($file);
+        $filename = $path['filename'];
+        $ext = $path['extension'];
+        $temp_name = $_FILES['image']['tmp_name'];
+        $path_filename_ext = $target_dir . $filename . "." . $ext;
 
-$connection = Connection::getInstance();
-
-$gateway = new LocationTableGateway($connection);
-
-$id = $gateway->update($location);
-
-header('Location: viewLocations.php');
+        unlink("uploads/$old_image");
+        move_uploaded_file($temp_name, $path_filename_ext);
+    } else {
+        $file = $old_image;
+    }
+    
+    $location = new Location($id, $locationName, $locationAddress, $managerFName, $managerLName, $managerEmail, $managerNumber, $locationMaxCap, $locationType, $seat, $facilities, $url, $file);
+    
+    $connection = Connection::getInstance();
+    
+    $gateway = new LocationTableGateway($connection);
+    
+    $id = $gateway->update($location);
+    
+    header('Location: viewLocations.php');
+    
+}
