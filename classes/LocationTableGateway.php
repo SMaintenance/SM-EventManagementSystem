2b0c3aff1c82f866f6719282fe9623b0cdf96bef
Humaira' AS <whomyras00@gmail.com>
2023-01-03 06:57:13 +0800

    Fix delete location error

--- classes/LocationTableGateway.php
+++ classes/LocationTableGateway.php
@@ -133,7 +133,19 @@ class LocationTableGateway {
     }
     
     public function delete($id) {
-        $sql = "DELETE FROM locations WHERE locationID = :id";
+        $sql = "DELETE FROM facilities WHERE LocationID = :id";
+        
+        $statement = $this->connect->prepare($sql);
+        $params = array(
+            "id" => $id
+        );
+        $status = $statement->execute($params);
+        
+        if (!$status) {
+            die("Could not delete facilities");
+        }
+
+        $sql = "DELETE FROM locations WHERE LocationID = :id";
         
         $statement = $this->connect->prepare($sql);
         $params = array(
