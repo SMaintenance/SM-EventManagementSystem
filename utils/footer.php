65f13d3cbfd2c932dd06b4675cdf9a8e74510c43
abibakrii <74251860+abibakrii@users.noreply.github.com>
2023-01-03 01:53:08 +0800

    Renew and redirect socmed icon

--- utils/footer.php
+++ utils/footer.php
@@ -27,10 +27,16 @@
             <section>
                 <div class = "footcontent col-md-4"><!--right content-->
                     Follow Us:<br>
-                        <img src = "images/facebook.png">
+                        <!--<img src = "images/facebook.png">
                         <img src = "images/twitter.png">
                         <img src = "images/googleplus.png">
-                        <img src = "images/youtube.png">
+                        <img src = "images/youtube.png"> -->
+                        <a href="https://www.youtube.com/"><img src="images/socmed/yt.png" height="45px" width="65px">
+                        <a href="https://www.facebook.com/"><img src = "images/socmed/facebook.png" height="60px" width="70px">
+                        <a href="https://www.twitter.com/"><img src = "images/socmed/twitter.png" height="50px" width="55px">
+                        <a href="https://www.instagram.com/"><img src = "images/socmed/instagram.png" height="50px" width="50px">
+                        
+                        
                 </div>
             </section>
         </div>
