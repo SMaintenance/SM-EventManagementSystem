40e3e53c3071d778785671f2870eee81bc74f2b9
Humaira' AS <whomyras00@gmail.com>
2023-01-07 21:04:43 +0800

    change style file

--- utils/styles.php
+++ utils/styles.php
@@ -2,6 +2,6 @@
 <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css.map">
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css.map">
-<link rel="stylesheet" type="text/css" href="css/new-style.css">
+<link rel="stylesheet" type="text/css" href="css/new-style-2.css">
 <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
 <link rel="icon" href="images/favicon.png" sizes="32x32" type="image/png">
