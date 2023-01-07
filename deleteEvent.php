79c459f39ce6c92869d38f8f8bb9038f9a773caa
haneyiskdr <haneyiskandar00@gmail.com>
2023-01-07 21:14:31 +0800

    Add delete confirmation modal

--- deleteEvent.php
+++ deleteEvent.php
@@ -3,16 +3,14 @@ require_once 'classes/Event.php';
 require_once 'classes/EventTableGateway.php';
 require_once 'classes/Connection.php';
 
+if (isset($_POST['submit'])) {
+    $id = $_POST['deleteId'];
 
-if (!isset($_GET['id'])) {
-    die("Illegal request");
+    $connection = Connection::getInstance();
+
+    $gateway = new EventTableGateway($connection);
+
+    $gateway->delete($id);
+
+    header('Location: viewEvents.php');
 }
-$id = $_GET['id'];
-
-$connection = Connection::getInstance();
-
-$gateway = new EventTableGateway($connection);
-
-$gateway->delete($id);
-
-header('Location: viewEvents.php');
